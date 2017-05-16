062262ca43790e139b0714eb466c509706f41dff
Sudasan <sudharshan.r@gmail.com>
2017-05-16 10:51:36 +0000

    Movie review project completed

--- Gemfile
+++ Gemfile
@@ -52,3 +52,8 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'devise', '~> 4.3'
+gem 'paperclip', '~> 5.1'
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'searchkick', '~> 2.3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
+  
+  resources :movies do
+    collection do
+      get 'search'
+    end  
+    resources :reviews, except: [:show, :index]  
+  end
+  
+  root 'movies#index'
 end
